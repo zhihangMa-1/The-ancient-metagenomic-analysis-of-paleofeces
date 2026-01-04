94843e7bf342e1f46cee340512a047d6d224933a
zhihangMa-1 <zhhangma@163.com>
2026-01-04 15:39:48 +0800

    Update fetch_data.sh

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -2,5 +2,10 @@
 # Description: Guide to download data from GSA
 # Accession: PRJCA055074
 
-echo "Please visit https://ngdc.cncb.ac.cn/gsa/browse/PRJCA039170 to download the FASTQ files."
+echo "--------------------------------------------------------"
+echo "Manuscript Data Access: PRJCA039170"
+echo "--------------------------------------------------------"
+echo "1. These data are hosted at: https://ngdc.cncb.ac.cn/gsa"
+echo "2. Due to journal and GSA policy, data will be public upon publication."
+echo "3. Once public, you can use the 'Aspera' or 'FTP' links provided by GSA."
 echo "Once downloaded, place them in the ./data folder and run scripts/01_preprocessing.sh"
