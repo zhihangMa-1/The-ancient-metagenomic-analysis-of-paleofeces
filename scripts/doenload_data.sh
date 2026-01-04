e5d8f43c68d77529a4a4059fc711f62240ab399e
zhihangMa-1 <zhhangma@163.com>
2026-01-04 17:08:41 +0800

    Update doenload_data.sh

--- scripts/doenload_data.sh
+++ scripts/doenload_data.sh
@@ -3,7 +3,7 @@
 # Accession: PRJCA055074
 
 echo "--------------------------------------------------------"
-echo "Manuscript Data Access: PRJCA039170"
+echo "Manuscript Data Access: PRJCA055074"
 echo "--------------------------------------------------------"
 echo "1. These data are hosted at: https://ngdc.cncb.ac.cn/gsa"
 echo "2. Due to journal and GSA policy, data will be public upon publication."
