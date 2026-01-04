55961e13b31e4a80b399cd4da6768609244bdbb4
zhihangMa-1 <zhhangma@163.com>
2026-01-04 15:10:36 +0800

    Update fetch_data.sh

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Description: Guide to download data from GSA
-# Accession: PRJCA039170
+# Accession: PRJCA055074
 
 echo "Please visit https://ngdc.cncb.ac.cn/gsa/browse/PRJCA039170 to download the FASTQ files."
 echo "Once downloaded, place them in the ./data folder and run scripts/01_preprocessing.sh"
